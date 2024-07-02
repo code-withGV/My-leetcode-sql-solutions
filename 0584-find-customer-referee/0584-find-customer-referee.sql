d90750f43ef520f10937ed0353f75c729e7ea145
Gaurav Vashistha <gauravvashistha452@gmail.com>
2024-07-02 18:17:17 +0530

    Update 0584-find-customer-referee.sql

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,9 @@
 # Write your MySQL query statement below
 select name from Customer where coalesce(referee_id,"")!= 2;
+
+## Explanation
+1. SELECT name: This command selects the name column from the Customers table.
+2. FROM Customers: Specifies the table to retrieve the data from.
+3. WHERE referee_id IS NOT NULL: Filters the results to include only those rows where the referee_id column is not null.
+##Important Function: COALESCE()
+In SQL, the COALESCE() function returns the first non-null value in a list of arguments. It is useful for handling null values in SQL queries.
