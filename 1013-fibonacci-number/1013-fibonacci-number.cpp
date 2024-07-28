da3cd3fdfca9a94522af701dbade43ff8dc1aa3e
Gaurav Vashistha <gauravvashistha452@gmail.com>
2024-07-28 11:36:39 +0530

    Time: 11 ms (46.93%), Space: 7.3 MB (32.61%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1) return n;
+        if(n<=1){
+            return n;
+        }
         int last = fib(n-1);
         int slast = fib(n-2);
-        return last+slast;
+
+        return last + slast;
     }
 };
